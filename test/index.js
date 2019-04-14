06f67e8f20176c32b4762511b467caaa72f071dc
John Watson <johnwatson484@hotmail.com>
2019-04-14 15:28:47 +0100

    Unit tests updated

--- test/index.js
+++ test/index.js
@@ -2,34 +2,92 @@ const Lab = require('lab')
 const Code = require('code')
 const lab = exports.lab = Lab.script()
 const createServer = require('../server')
+const sinon = require('sinon')
+const userService = require('../server/services/user-service')
+const claimService = require('../server/services/claim-service')
 
 lab.experiment('API test', () => {
   let server
+  let sandbox
+
+  async function returnTrue (obj) {
+    return true
+  }
+
+  async function returnFalse (obj) {
+    return false
+  }
 
   // Create server before each test
   lab.before(async () => {
     server = await createServer()
+    sandbox = sinon.createSandbox()
   })
 
-  lab.test('GET / route works', async () => {
+  lab.afterEach(async () => {
+    sandbox.restore()
+  })
+
+  lab.test('POST /claim route works', async () => {
     const options = {
-      method: 'GET',
-      url: '/'
+      method: 'POST',
+      url: '/claim',
+      payload: {
+        claimId: 'MINE123',
+        propertyType: 'business',
+        accessible: false,
+        dateOfSubsidence: new Date(2015, 5, 6),
+        mineType: ['gold', 'iron'],
+        email: 'test@email.com'
+      }
     }
+
+    sandbox.stub(userService, 'register').callsFake(returnTrue)
+    sandbox.stub(claimService, 'submit').callsFake(returnTrue)
 
     const response = await server.inject(options)
     Code.expect(response.statusCode).to.equal(200)
-    Code.expect(response.result).to.equal({ hello: 'world' })
   })
 
-  lab.test('GET /about route works', async () => {
+  lab.test('POST /claim route returns 503 if user service unavailable', async () => {
     const options = {
-      method: 'GET',
-      url: '/about'
+      method: 'POST',
+      url: '/claim',
+      payload: {
+        claimId: 'MINE123',
+        propertyType: 'business',
+        accessible: false,
+        dateOfSubsidence: new Date(2015, 5, 6),
+        mineType: ['gold', 'iron'],
+        email: 'test@email.com'
+      }
     }
 
+    sandbox.stub(userService, 'register').callsFake(returnFalse)
+    sandbox.stub(claimService, 'submit').callsFake(returnTrue)
+
     const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(200)
-    Code.expect(response.result).to.equal({ ok: 200 })
+    Code.expect(response.statusCode).to.equal(503)
+  })
+
+  lab.test('POST /claim route returns 503 if claim service unavailable', async () => {
+    const options = {
+      method: 'POST',
+      url: '/claim',
+      payload: {
+        claimId: 'MINE123',
+        propertyType: 'business',
+        accessible: false,
+        dateOfSubsidence: new Date(2015, 5, 6),
+        mineType: ['gold', 'iron'],
+        email: 'test@email.com'
+      }
+    }
+
+    sandbox.stub(userService, 'register').callsFake(returnTrue)
+    sandbox.stub(claimService, 'submit').callsFake(returnFalse)
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(503)
   })
 })
